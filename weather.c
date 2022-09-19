61e3b3abcc31a4e631f157a52421b7072ae4606f
Nate Cater <ncater3@gatech.edu>
2022-09-18 20:05:19 -0700

    final version

--- weather.c
+++ weather.c
@@ -3,19 +3,24 @@
 
 int main() {
 
+    //create array to store rainfall over 12 months for 5 years
     float rainfall[5][12] = {
         {1.2, 4.3, 9.1, 1.1, 5.1, 3.7, 8.3, 2.1, 2.9, 1.5, 0.0, 10.1}, 
         {5.2, 1.1, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 5.3, 5.6, 5.7},
         {2.3, 1.2, 3.4, 4.5, 3.3, 2.2, 1.1, 6.6, 7.7, 8.8, 9.9, 1.1},
         {0.0, 0.0, 0.0, 0.0, 5.4, 5.4, 6.7, 6.9, 6.9, 4.2, 4.2, 1.1},
         {1.1, 9.9, 9.6, 10.1, 11.2, 12.3, 15.4, 20.4, 100.4, 11.2, 1.1}
-    }
+    };
 
+    /*one var to store overall year average, a temporary sum for loops
+      two arrays - one for each month's average and one for the total each yr
+    */
     float mon_avg[12] = {};
     float yr_avg = 0.0;
     float yr_total[5] = {};
     float sum = 0.0;
 
+    //this loop is to fill out the yearly total array for each 5 years
     for (int i = 0; i < 5; ++i) {
 
         sum = 0.0;
@@ -27,12 +32,14 @@ int main() {
         yr_total[i] = sum;
     }
 
+    //this loop calculates the overal yearly average
     for (int i = 0; i < 5; ++i) {
         yr_avg += yr_total[i];
     }
 
     yr_avg = yr_avg/5;
 
+    //calculate the monthly average
     for (int i = 0; i < 12; ++i) {
 
         sum = 0.0;
@@ -44,19 +51,20 @@ int main() {
         mon_avg[i] = sum/12;
     }
 
+    // output
     printf("YEAR  RAINFALL (inches)\n");
 
     for(int i = 0; i <5; ++i) {
-        printf("201%d  %f", i, yr_total[i]);
+        printf("201%d  %.1f", i, yr_total[i]);
         printf("\n");
     }
 
-    printf("\nThe Yearly average is %f inches.\n", yr_avg);
+    printf("\nThe Yearly average is %.1f inches.\n", yr_avg);
     printf("\nMONTHLY AVERAGES:\n\n");
-    printf("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec");
+    printf("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec\n");
 
     for(int i = 0; i< 12; ++i) {
-        printf("%f ", mon_avg[i]);
+        printf("%.1f ", mon_avg[i]);
     }
 
 }
